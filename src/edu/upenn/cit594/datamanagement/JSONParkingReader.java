d300a39729d98fb4b8acbbb5c8ff827ddbeef053
AmberChaoYang <chaoyang@seas.upenn.edu>
2019-12-05 21:42:16 -0500

    N-Tier Architecture design
    based on the draft UML graph

---
+++ src/edu/upenn/cit594/datamanagement/JSONParkingReader.java
@@ -0,0 +1,18 @@
+package edu.upenn.cit594.datamanagement;
+
+import java.util.ArrayList;
+
+import edu.upenn.cit594.data.Parking;
+
+
+public class JSONParkingReader implements Reader{
+
+	
+	public ArrayList<Parking> read() {
+		
+		return null;
+		
+	}
+	
+
+}
