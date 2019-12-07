54edc724b289251bb6d702e7ce0f8074ae84aa37
esbickel <46583340+esbickel@users.noreply.github.com>
2019-12-07 17:23:05 -0500

    Update PopulationReader.java

--- src/edu/upenn/cit594/datamanagement/PopulationReader.java
+++ src/edu/upenn/cit594/datamanagement/PopulationReader.java
@@ -1,12 +1,15 @@
 package edu.upenn.cit594.datamanagement;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import edu.upenn.cit594.data.Population;
 
 public interface PopulationReader {
 
-		public ArrayList<Population> read();
-
+	
+	public HashMap<String, Integer> read();
+		
 		
 	}
 
