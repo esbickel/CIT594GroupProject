93d5646ce99d33e344732cdbcead148147406e7d
esbickel <46583340+esbickel@users.noreply.github.com>
2019-12-07 17:22:28 -0500

    Update Population.java

--- src/edu/upenn/cit594/data/Population.java
+++ src/edu/upenn/cit594/data/Population.java
@@ -1,7 +1,52 @@
 package edu.upenn.cit594.data;
 
-public class Population {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
+import edu.upenn.cit594.logging.Logger;
+
+public class Population {
+	
+	private static HashMap<String, Integer> populations;
+	
+	// Constructor
+	private Population() {
+		populations = new HashMap <String, Integer>();
+	}
+	
+	//Instance
+	public static Population instance = new Population();
+	
+	
+	//Get methods
+	public void add(String zip, String pop) {
+		populations.put(zip, Integer.parseInt(pop));
+	}
+	
+	public static Integer getPopulation(String Zip) {
+		return populations.get(Zip);
+	}
+	
+	public static Set<String> getZipCodes() {
+		return(populations.keySet());
+	}
+	
+	public static Population getInstance() {
+		return instance;
+	}
+	
+	public static HashMap<String, Integer> get(){
+		return populations;
+	}
+	
+	
+	
+	/*
 	int population;
 	int zipCode;
 	
@@ -24,7 +69,7 @@ public class Population {
 	public void setZipCode(int zipCode) {
 		this.zipCode = zipCode;
 	}
-	
+	*/
 	
 	
 }
