a2803737c03f5577dcab089163f9efbbc6f48476
esbickel <46583340+esbickel@users.noreply.github.com>
2019-12-07 17:21:34 -0500

    Update Main.java

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -1,5 +1,29 @@
 package edu.upenn.cit594;
 
-public class Main {
+import edu.upenn.cit594.logging.Logger;
 
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		if (args.length != 5) {
+			System.out.println("invalid args");
+			System.exit(0);
+		}
+		if (!(args[0].equals("csv") || args[0].equals("json"))) {
+			System.out.println("invalid args");
+			System.exit(0);
+		}
+		
+		String ParkingFileFormat = args[0];
+		String ParkingFile = args[1];
+		String PropertyFile = args[2];
+		String PopulationFile = args[3];	
+		String LogFile = args[4];
+		
+		Logger l = Logger.getInstance(LogFile);
+		l.log(args);
+		
+		
+	}
 }
