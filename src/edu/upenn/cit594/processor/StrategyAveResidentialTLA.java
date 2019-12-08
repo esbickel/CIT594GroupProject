6201ca96eca7c4286e857f250013e062ac140d03
esbickel <46583340+esbickel@users.noreply.github.com>
2019-12-07 20:54:14 -0500

    Update StrategyAveResidentialTLA.java

--- src/edu/upenn/cit594/processor/StrategyAveResidentialTLA.java
+++ src/edu/upenn/cit594/processor/StrategyAveResidentialTLA.java
@@ -1,5 +1,24 @@
 package edu.upenn.cit594.processor;
 
-public class StrategyAveResidentialTLA {
+import java.util.ArrayList;
 
+import edu.upenn.cit594.data.Properties;
+import edu.upenn.cit594.datamanagement.CSVPropertiesReader;
+import edu.upenn.cit594.datamanagement.PropertiesReader;
+
+public class StrategyAveResidentialTLA {
+	
+	public double attributeSum(Integer zip, ArrayList<Properties> props) {
+		
+		double TLAsum = 0;
+		
+		for (Properties prop : props) {
+			if (prop.getZipCode() == zip) {
+				TLAsum += prop.getTotalLivableArea();
+			}
+		} 
+		
+		return TLAsum;
+	}
+	
 }
