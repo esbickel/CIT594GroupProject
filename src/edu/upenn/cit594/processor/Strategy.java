03d36a29df2eb82fd0f9ae88c27bbe4c797d8104
esbickel <46583340+esbickel@users.noreply.github.com>
2019-12-07 20:53:21 -0500

    Update Strategy.java

--- src/edu/upenn/cit594/processor/Strategy.java
+++ src/edu/upenn/cit594/processor/Strategy.java
@@ -1,5 +1,12 @@
 package edu.upenn.cit594.processor;
 
-public interface Strategy {
+import java.util.ArrayList;
 
+import edu.upenn.cit594.data.Properties;
+
+public interface Strategy {
+	
+	public double attributeSum(Integer zip, ArrayList<Properties> props);
+	
 }
+
