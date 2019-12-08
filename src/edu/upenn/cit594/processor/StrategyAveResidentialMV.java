972ff2e1443c64d573f3476b9324d49ab9591915
esbickel <46583340+esbickel@users.noreply.github.com>
2019-12-07 20:53:54 -0500

    Update StrategyAveResidentialMV.java

--- src/edu/upenn/cit594/processor/StrategyAveResidentialMV.java
+++ src/edu/upenn/cit594/processor/StrategyAveResidentialMV.java
@@ -1,5 +1,24 @@
 package edu.upenn.cit594.processor;
 
-public class StrategyAveResidentialMV {
+import java.util.ArrayList;
 
+import edu.upenn.cit594.data.Properties;
+import edu.upenn.cit594.datamanagement.CSVPropertiesReader;
+import edu.upenn.cit594.datamanagement.PropertiesReader;
+
+public class StrategyAveResidentialMV {
+	
+	public double attributeSum(Integer zip, ArrayList<Properties> props) {
+		
+		double MVsum = 0;
+		
+		for (Properties prop : props) {
+			if (prop.getZipCode() == zip) {
+				MVsum += prop.getMarketValue();
+			}
+		}
+		
+		return MVsum;
+	}
+	
 }
