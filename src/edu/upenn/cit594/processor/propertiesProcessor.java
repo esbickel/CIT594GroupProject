a10bf5fe9cbdd85c35008b96b75b0be626fa617d
esbickel <46583340+esbickel@users.noreply.github.com>
2019-12-07 20:52:35 -0500

    Update propertiesProcessor.java

--- src/edu/upenn/cit594/processor/propertiesProcessor.java
+++ src/edu/upenn/cit594/processor/propertiesProcessor.java
@@ -1,5 +1,47 @@
 package edu.upenn.cit594.processor;
 
-public class propertiesProcessor {
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Set;
 
+import edu.upenn.cit594.data.Population;
+import edu.upenn.cit594.data.Properties;
+import edu.upenn.cit594.datamanagement.CSVPropertiesReader;
+import edu.upenn.cit594.datamanagement.PopulationReader;
+import edu.upenn.cit594.datamanagement.PropertiesReader;
+import edu.upenn.cit594.datamanagement.TXTPopulationReader;
+
+public class propertiesProcessor {
+	
+	String strategy;
+	
+	public double PropertyAverage(Strategy strategy, String fileName) {
+		
+		Scanner kbIn = new Scanner(System.in);
+		Set<String> ZipCodes = Population.getZipCodes();
+		String zip = "?";
+		
+		while (!ZipCodes.contains(zip)) {
+			System.out.println("Enter a valid ZIP code:  ");
+			zip = kbIn.next();
+		}
+		int intZIP = Integer.parseInt(zip);
+		
+		PropertiesReader pr = new CSVPropertiesReader("properties.csv");
+		ArrayList<Properties> props = pr.read();
+		if(props == null) {return -1;}
+		
+		double attributeTotal = strategy.attributeSum(intZIP, props);
+		
+		int propCount = 0;
+		for (Properties prop : props) {
+			if (prop.getZipCode() == intZIP) {
+				propCount++;
+			}
+		}
+		
+		double average = attributeTotal/propCount;
+		
+		return average;
+	}
 }
